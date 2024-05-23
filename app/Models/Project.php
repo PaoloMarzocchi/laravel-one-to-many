eaf9bfdb8ddd91b70464126b01282b52554eb0ec
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-05-23 15:56:54 +0200

    add type model with controller,requests and seeder, add relations in models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
@@ -12,4 +13,14 @@ class Project extends Model
     protected $fillable = [
         'title', 'slug', 'project_url', 'preview', 'repo_url', 'start_date', 'end_date', 'description'
     ];
+
+    /**
+     * Get the type that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
